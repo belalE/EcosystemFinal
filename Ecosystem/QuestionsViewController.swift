de17ef5b1ff01f9249088800038fefde5ead8c66
Belal E <balolyman1234@gmail.com>
2017-05-20 14:27:55 +0300

    project done

--- Ecosystem/QuestionsViewController.swift
+++ Ecosystem/QuestionsViewController.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
     
     var score = 0
     
     var clicks = 0
     
-    @IBOutlet weak var button1: UIButton!
-    @IBOutlet weak var button2: UIButton!
-    @IBOutlet weak var button3: UIButton!
-    
+  
     @IBOutlet weak var QuestionLabel: UILabel!
     
     
@@ -42,6 +42,8 @@ class QuestionsViewController: UIViewController {
             }
             
         }
+        
+        
         if button1.layer.borderWidth == 5 {
             if QuestionLabel.text == "What are the main types of symbiosis?" {
                 score = score + 1
@@ -54,41 +56,47 @@ class QuestionsViewController: UIViewController {
             
             QuestionLabel.text = "Which would be considered an adaptation?"
             
-            button1.titleLabel?.text = "Mosquitos drinking blood"
-            button2.titleLabel?.text = "Birds eating tics on rhinos"
-            button3.titleLabel?.text = "Sharks being able to smell blood underwater"
+            button1.setTitle("Mosquitos drinking blood", for: [])
+            button2.setTitle("Birds eating tics on rhinos", for: [])
+            button3.setTitle("Sharks being able to smell blood underwater", for: [])
             
         }
         if clicks == 2 {
             QuestionLabel.text = "What are the main types of symbiosis?"
             
-            button1.titleLabel?.text = "Mutualism, Parasitism, Commensalism and Amensalism"
-            button2.titleLabel?.text = "Mutualism, Parasitism, Commensalism and Antibiosis"
-            button3.titleLabel?.text = "Parasitism, Commensalism, Amensalism and Antibiosis"
+            button1.setTitle("Mutualism, Parasitism, Commensalism and Amensalism", for: [])
+            button2.setTitle("Mutualism, Parasitism, Commensalism and Antibiosis", for: [])
+            button3.setTitle("Parasitism, Commensalism, Amensalism and Antibiosis", for: [])
         }
         if clicks == 3 {
             QuestionLabel.text = "What would happen if the population of the tertiary consumer  went down?"
             
-            button1.titleLabel?.text = "The population of the secondary consumer increases"
-            button2.titleLabel?.text = "The population of the primary consumer increases"
-            button3.titleLabel?.text = "The population of the secondary consumer decreases"
+            button1.setTitle("The population of the secondary consumer increases", for: [])
+            button2.setTitle("The population of the primary consumer increases", for: [])
+            button3.setTitle("The population of the secondary consumer decreases", for: [])
         }
         if clicks == 4 {
             QuestionLabel.text = "Why is biodiversity important?"
             
-            button1.titleLabel?.text = "It helps the ecosystem survive"
-            button2.titleLabel?.text = "It helps the population grow"
-            button3.titleLabel?.text = "It increases stability of the ecosystem"
+            button1.setTitle("It helps the ecosystem survive", for: [])
+            button2.setTitle("It helps the population grow", for: [])
+            button3.setTitle("It increases stability of the ecosystem", for: [])
         }
         if clicks == 5 {
             QuestionLabel.text = "What is a carrying capacity?"
             
-            button1.titleLabel?.text = "The amount of resources available"
-            button2.titleLabel?.text = "The amount of organisms in the ecosystem"
-            button3.titleLabel?.text = "The amount of organisms the ecosystem can support"
+            button1.setTitle("The amount of resources available", for: [])
+            button2.setTitle("The amount of organisms in the ecosystem", for: [])
+            button3.setTitle("The amount of organisms the ecosystem can support", for: [])
+            
+            
         }
         if clicks == 6 {
-            performSegue(withIdentifier: "ScoreSegue", sender: score)
+            QuestionLabel.text = "All Done!"
+            
+            button1.setTitle("Score:", for: [])
+            button2.setTitle(String(score), for: [])
+            button3.setTitle("of 6", for: [])
         }
         
         button1.layer.borderWidth = 0
@@ -105,11 +113,9 @@ class QuestionsViewController: UIViewController {
         
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let SVC = segue.destination as! ScoreViewController
-        SVC.displayScore = sender! as! String
-        
-    }
+    
+  
+
     
     
     @IBAction func ans2(_ sender: Any) {
@@ -127,13 +133,18 @@ class QuestionsViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        
+        
         QuestionLabel.text = "What is the order of the flow of energy in an energy pyramid from bottom to top?"
         
-        button1.titleLabel?.text = "Primary Consumer,Tertiary Consumer,Secondary Consumer,Producer,Decomposer"
+        button1.setTitle("Primary Consumer,Tertiary Consumer,Secondary Consumer,Producer,Decomposer", for: [])
         
-        button2.titleLabel?.text =  "Producer,Primary Consumer,Secondary Consumer,Tertiary Consumer,Decomposer."
+        button2.setTitle("Producer,Primary Consumer,Secondary Consumer,Tertiary Consumer,Decomposer.", for: [])
         
-        button3.titleLabel?.text  = "Producer,Secondary Consumer,Tertiary Consumer, Decomposer"
+        button3.setTitle("Producer,Secondary Consumer,Tertiary Consumer, Decomposer"
+            , for: [])
+        
+        
         
     }
     
